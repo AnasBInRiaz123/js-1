cfa23b9858518ba9bdf82aa8b4451f5b4833601d
AnasBinRiaz123 <anasbinriaz@gmail.com>
2021-03-16 20:29:33 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,230 @@
+// CHAPTER N0. 1
+// Ques 1
+
+// code
+alert("Greetings! Welcome")
+
+
+// Ques 2
+
+// code
+alert("Error! Please Enter A Valid Password")
+
+
+// Ques 3
+
+// code
+alert("Welcome to JS Land... \n HAPPY CODING")
+
+
+// Ques 4
+
+// code
+alert("Welcome to JS Land...")
+alert("HAPPY CODING \n Prevent this page from creating additional dialog")
+
+
+// Ques 5
+
+// code
+alert("Hello... I can run JS through my web browsers console`")
+
+
+
+// CHAPTER N0. 2
+// Ques 1
+
+// code
+var username;
+
+
+// Ques 2
+
+// code
+var  myName = "Anas Bin Riaz"
+
+
+// Ques 3
+
+// code
+var message;
+message = "Hello World"
+alert(message)
+
+
+// Ques 4
+
+// code
+var Name = "Anas Bin Riaz";
+var age="19 years old";
+var certification = "Web And Mobile Certified"
+alert(Name)
+alert(age)
+alert(certification)
+
+
+// Ques 5
+
+// code
+alert("PIZZA \nPIZZ \nPIZ \nPI \nP")
+
+
+// Ques 6
+
+// code
+var email;
+email = "example@gmail.com"
+var text = "My email address is "
+alert(text + email)
+
+
+
+// Ques 7
+
+// code
+var book;
+book = "A smarter way to learn JavaScript"
+var text = "I am trying to learn from the "
+alert(text + book)
+
+
+// Ques 8
+
+// code
+document.write("Yah! I can write HTML content in JAVASCRIPT")
+
+
+
+// Ques 9
+
+// code
+var border = " ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ " 
+
+
+
+// CHAPTER N0. 3
+// Ques 1
+
+// code
+var age = 19;
+alert(" I am " + age +  " years old")
+
+
+
+// Ques 2
+
+// code
+var visited =+prompt("Your Visted Number")
+alert("you have visited the site " + visited + " times") 
+
+
+
+// Ques 3
+
+// code
+var birthyear = 2000;
+document.write("my birth year is " + birthyear + "\n Data type of my declared varialble is numbers" )
+
+
+
+
+// Ques 4
+
+// code
+var visitors= prompt("Your Name")
+var Product=prompt("Product title")
+var quantity=prompt(" Desired Quantity")
+docoument.write(visitors + "  ordered" + quantity + " " + Product + "(s) on XYZ Clothing store" )
+
+
+
+// CHAPTER N0. 4
+// Ques 1
+
+// code
+var a= 5;
+var b= 10;
+var c= a+b;
+
+
+// Ques 2
+
+// code
+alert("a,anas, anas_1, anas1, AnAs \nthese all are legal variable \n \n 1, 1a, 1_anas, <anas, an as \nthese all are illegal variable")
+
+
+// Chapter no. 5
+
+// Ques. 1
+
+
+var a = 3;
+var b = 5;
+var c = a + b
+document.write("sum of " + a + " and " + b + " is " + c)
+
+// Ques. 2
+
+
+var a = 3;
+var b = 5;
+var c = a - b
+document.write("  Difference of " + a + " and " + b + " is " + c)
+
+
+
+var a = 3;
+var b = 5;
+var c = a * b
+document.write("  product of " + a + " and " + b + " is " + c)
+
+
+
+
+var a = 3;
+var b = 5;
+var c = a / b
+document.write("  division of " + a + " and " + b + " is " + c)
+
+
+var a = 3;
+var b = 5;
+var c = a % b
+document.write("  Modulus of " + a + " and " + b + " is " + c)
+
+
+// Ques. 4
+
+
+var ticket = prompt("No of Tickets")
+
+if (ticket = 1 && ticket<100) {
+    document.write(600)
+}
+if (ticket = 2 && ticket<100) {
+    document.write(1200)
+}
+if (ticket = 3 && ticket<100) {
+    document.write(1800)
+}
+if (ticket = 4 && ticket<100) {
+    document.write(2400)
+}
+if (ticket = 5 && ticket<100) {
+    document.write(3000)
+}
+if (ticket = 6 && ticket<100) {
+    document.write(3600)
+}
+if (ticket = 7 && ticket<100) {
+    document.write(4200)
+}
+if (ticket = 8 && ticket<100) {
+    document.write(4800)
+}
+if (ticket = 9 && ticket<100) {
+    document.write(5400)
+}
+if(ticket=10 && ticket<100 ){
+    document.write(6000)
+    }
